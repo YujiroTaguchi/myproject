d8d796b26204831b5f75442a1cb5582a00a14133
YujiroTaguchi <no11yujiro@hotmail.co.jp>
2024-07-18 21:20:14 +0900

    0178 2120

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5hnq_nbmg1_oo$+uk_nqscyzq--)1ql%gzwy4-fqb6x*pgzkf&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
