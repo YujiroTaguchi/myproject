cc3c040e03aacc94dd7caf6e91a61954710080ba
YujiroTaguchi <no11yujiro@hotmail.co.jp>
2024-07-20 14:07:45 +0900

    0720 1407

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER' : os.getenv('USER'),
         'PASSWORD':os.getenv('PASSWORD'),
         'HOST' : os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+        'PORT': '3306',
     }
 }
 
