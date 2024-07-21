05c9bbb61aee39de19c1842bfcb230e5485a9fd5
YujiroTaguchi <no11yujiro@hotmail.co.jp>
2024-07-21 12:26:30 +0900

    0721 1226

--- myproject/urls.py
+++ myproject/urls.py
@@ -32,6 +32,7 @@ from nagoyameshi.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RestaurantListView.as_view(), name='home'),  # ルートアクセス用のURLパターンを追加
     path('restaurants/', RestaurantListView.as_view(), name='restaurant_list'),
     path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant_detail'),
     path('signup/', signup, name='signup'),
