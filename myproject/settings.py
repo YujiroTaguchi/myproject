312a967ad6c76c8a9d7fd176ac49705848d1ce64
YujiroTaguchi <no11yujiro@hotmail.co.jp>
2024-07-22 15:30:19 +0900

    S3設定

--- myproject/settings.py
+++ myproject/settings.py
@@ -178,6 +178,13 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_DEFAULT_ACL = 'public-read'
+
 # ファイルストレージの設定
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
